0494ea20d3c3f2b4e4a7e59c74a58730fcea8169
Kenneth <kenships0402@gmail.com>
2025-12-23 23:18:29 -0500

    Settings volume now works

--- Assets/_Project/Scripts/Core/SoundPooling/ScriptableObject/VolumeSO.cs
+++ Assets/_Project/Scripts/Core/SoundPooling/ScriptableObject/VolumeSO.cs
@@ -1,14 +1,93 @@
+using System;
 using Obvious.Soap;
+using PrimeTween;
+using Sisus.Init;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace _Project.Scripts.Core.SoundPooling.ScriptableObject
 {
+    [Service(typeof(VolumeSO), ResourcePath = "ScriptableObjects/Audio/VolumeSO")]
     [CreateAssetMenu(fileName = "VolumeSO", menuName = "Scriptable Objects/Audio/VolumeSO")]
     public class VolumeSO : UnityEngine.ScriptableObject
     {
-        public BoolVariable mute;
-        public FloatVariable masterVolume;
-        public FloatVariable musicVolume;
-        public FloatVariable sfxVolume;
+        [SerializeField] private BoolVariable mute;
+        [SerializeField] private FloatVariable masterVolume;
+        [SerializeField] private FloatVariable musicVolume;
+        [SerializeField] private FloatVariable sfxVolume;
+        [SerializeField] private AudioMixer mixer;
+
+        public bool Mute
+        {
+            get => mute.Value;
+            set => mute.Value = value;
+        }
+        public float MasterVolume {
+            get => masterVolume.Value;
+            set => masterVolume.Value = value;
+        }
+        public float MusicVolume {
+            get => musicVolume.Value;
+            set => musicVolume.Value = value;
+        }
+        public float SfxVolume {
+            get => sfxVolume.Value;
+            set => sfxVolume.Value = value;
+        }
+        
+       
+
+        private void OnEnable()
+        {
+            masterVolume.OnValueChanged += MasterVolumeOnValueChanged;
+            musicVolume.OnValueChanged += MusicVolumeOnValueChanged;
+            sfxVolume.OnValueChanged += SfxVolumeOnValueChanged;
+            mute.OnValueChanged += MuteOnValueChanged;
+            InitializeMixer();
+        }
+
+        private void OnDisable()
+        {
+            masterVolume.OnValueChanged -= MasterVolumeOnValueChanged;
+            musicVolume.OnValueChanged -= MusicVolumeOnValueChanged;
+            sfxVolume.OnValueChanged -= SfxVolumeOnValueChanged;
+            mute.OnValueChanged -= MuteOnValueChanged;
+        }
+
+        public void InitializeMixer()
+        {
+            MuteOnValueChanged(Mute);
+            MasterVolumeOnValueChanged(MasterVolume);
+            MusicVolumeOnValueChanged(MusicVolume);
+            SfxVolumeOnValueChanged(SfxVolume);
+        }
+        
+        private void MuteOnValueChanged(bool isMute)
+        {
+            mixer.SetFloat("Master", LinearToDb(0));
+        }
+
+        private void SfxVolumeOnValueChanged(float volume)
+        {
+            mixer.SetFloat("Sfx", LinearToDb(volume));
+        }
+
+        private void MusicVolumeOnValueChanged(float volume)
+        {
+            mixer.SetFloat("Music", LinearToDb(volume));
+        }
+
+        private void MasterVolumeOnValueChanged(float volume)
+        {
+            mixer.SetFloat("Master", LinearToDb(volume));
+        }
+
+        private float LinearToDb(float volume)
+        {
+            Math.Clamp(volume, 0.0001f, 100f);
+            float volumePercent = volume / 100f;
+
+            return Mathf.Log10(volumePercent) * 20f;
+        }
     }
 }
---
+++ Assets/_Project/Scripts/Core/SoundPooling/VolumeInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using _Project.Scripts.Core.SoundPooling.ScriptableObject;
+using Sisus.Init;
+using UnityEngine;
+
+namespace _Project.Scripts.Core.SoundPooling
+{
+    public class VolumeInitializer : MonoBehaviour<VolumeSO>
+
+    {
+        private VolumeSO _volume;
+        protected override void Init(VolumeSO volume)
+        {
+            _volume = volume;
+        }
+
+        private void Start()
+        {
+            _volume.InitializeMixer();
+        }
+    }
+}
